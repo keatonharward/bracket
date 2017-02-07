f6fda60ae42280c1d30caea1b2e40afab37ce3ba
Keaton Harward <keatonharward@Keatons-MacBook-Pro-2.local>
2017-02-07 11:21:51 -0700

    Added team model

---
+++ Bracket/Team.swift
@@ -0,0 +1,16 @@
+//
+//  Team.swift
+//  Bracket
+//
+//  Created by Keaton Harward on 2/7/17.
+//  Copyright © 2017 KeatonHarward. All rights reserved.
+//
+
+import Foundation
+
+struct Team {
+    
+    let name: String
+    var gamesPlayed: [Int:Bool]?
+    
+}
